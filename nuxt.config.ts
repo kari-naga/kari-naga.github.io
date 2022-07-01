3a5f56391a8579a05c53ddb1449775ad44b49d88
Kari Naga <4119937+kari-naga@users.noreply.github.com>
2022-07-01 17:23:09 -0400

    Update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,14 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   typescript: {
-    shim: false
-  }
+    shim: false,
+  },
+  modules: [
+    '@nuxtjs/tailwindcss',
+  ],
+  tailwindcss: {
+    config: {
+      plugins: [],
+    },
+  },
 })
