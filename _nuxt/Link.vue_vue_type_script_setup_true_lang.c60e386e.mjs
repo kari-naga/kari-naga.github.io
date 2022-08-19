acd0f846e428152c3b851cd7aadeefc0a2076258
Kari Naga <4119937+kari-naga@users.noreply.github.com>
2022-08-18 22:09:08 -0400

    Updates

--- _nuxt/Link.vue_vue_type_script_setup_true_lang.c60e386e.mjs
+++
@@ -1,1 +0,0 @@
-import{e as n,o as s,c as a,w as c,i as r,s as _}from"./entry.7b13b1d7.mjs";const p=n({__name:"Link",props:["to"],setup(t){return(o,i)=>{const e=_;return s(),a(e,{to:t.to,class:"transition-colors hover:text-blue-500 active:text-blue-700"},{default:c(()=>[r(o.$slots,"default")]),_:3},8,["to"])}}});export{p as _};
