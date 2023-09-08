39ff57d221258c6dfefedacdfa4175547e264c42
Kari Naga <4119937+kari-naga@users.noreply.github.com>
2023-09-08 19:34:12 -0400

    Update secret

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
       },
       keyframes: {
         text: {
-          '0%': {
+          '0%, 100%': {
             'background-size': '200% 200%',
             'background-position': 'left top',
           },
